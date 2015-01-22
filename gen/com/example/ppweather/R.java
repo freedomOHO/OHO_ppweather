c4265734f4b5dc0a06c6cbc04d3fc593b9627b8e
freedomOHO <837588961@qq.com>
2015-01-23 00:52:51 +0800

    add the db class

--- gen/com/example/ppweather/R.java
+++ gen/com/example/ppweather/R.java
@@ -995,13 +995,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f05003c;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int container=0x7f05003c;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1096,9 +1095,7 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_main=0x7f030017;
-        public static final int fragment_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f030017;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
---
+++ src/com/ppweather/app/db/PPWeatherOpenHelper.java
@@ -0,0 +1,47 @@
+package com.ppweather.app.db;
+
+
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class PPWeatherOpenHelper extends SQLiteOpenHelper {
+	
+	
+	public static final String CREATE_PROVINCE = "create table Province ("
+				+ "id integer primary key autoincrement, " 
+				+ "province_name text, "
+				+ "province_code text)";
+	
+	public static final String CREATE_CITY = "create table City ("
+				+ "id integer primary key autoincrement, " 
+				+ "city_name text, " 
+				+ "city_code text, " 
+				+ "province_id integer)";
+	
+	public static final String CREATE_COUNTY = "create table County ("
+				+ "id integer primary key autoincrement, " 
+				+ "county_name text, " 
+				+ "county_code text, " 
+				+ "city_id integer)";
+
+	public PPWeatherOpenHelper(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(CREATE_PROVINCE); 
+		db.execSQL(CREATE_CITY);  
+		db.execSQL(CREATE_COUNTY);  
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	}
+
+}
